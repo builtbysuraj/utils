78c55201e2a28c7d20c2372b53bc2fee00a601ee
builtbysuraj <surajgupta3553@gmail.com>
2024-02-27 21:09:26 +0530

    Add multiply

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
-export function add(initialValue = 0, ...rest: number[]) {
-  return rest.reduce((a, b) => a + b, initialValue)
+export function add(...rest: number[]) {
+  return rest.reduce((a, b) => a + b, 0)
+}
+
+export function multiply(a: number, b: number) {
+  return a * b
 }
