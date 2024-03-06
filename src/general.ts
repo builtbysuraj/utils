65fb082017c9f183bd3dfa8a71dd653c12fb70a5
builtbysuraj <surajgupta3553@gmail.com>
2024-03-06 19:27:27 +0530

    feat: Add FormData util

--- src/general.ts
+++ src/general.ts
@@ -12,3 +12,10 @@ export function id() {
 export function cn(...classes: ClassesType[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+export function getFormData(evt: Event | React.FormEvent<HTMLFormElement>) {
+  const form = (evt.target || evt.currentTarget) as HTMLFormElement
+  const formData = new FormData(form)
+  const data = Object.fromEntries(formData)
+  return { data, form }
+}
